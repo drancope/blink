7d053dfa6aa68df1fb298656fbeea7144b1dd4b6
drancope <drancope@gmail.com>
2025-03-11 10:06:41 +0100

    commit inicial

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#define LED 13
+
+void setup() {
+  pinMode(LED, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(LED, HIGH);
+  delay(100);
+  digitalWrite(LED, LOW);
+  delay(1000);
+  Serial.print("funciona.");
+}
